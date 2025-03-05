cacb9f404bf7f143f61379a3a7bce519c5c744de
MKeys-Capstone <mkeys@capstone-is.com>
2025-03-05 15:25:42 -0500

    svgicon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatIconModule } from '@angular/material/icon';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   imports: [MatProgressBarModule, MatIconModule],
+  providers: [HttpClient],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(
+    private iconRegistry: MatIconRegistry,
+    private sanitizer: DomSanitizer
+  ) {
+    this.iconRegistry.addSvgIcon(
+      'custom-pdf', // Icon name
+      this.sanitizer.bypassSecurityTrustResourceUrl('assets/svg/pdf.svg') // Path to your SVG
+    );
+  }
+
+  ngOnInit(): void {}
+}
